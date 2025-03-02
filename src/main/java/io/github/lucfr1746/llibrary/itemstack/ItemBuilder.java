6885c0dbbf17c2648a16bf3bb5414dfe71fe01a8
Thế Lực <LucFr.1746@gmail.com>
2025-03-03 00:42:37 +0700

    Fixed ItemBuilder Minimessage error

--- src/main/java/io/github/lucfr1746/llibrary/itemstack/ItemBuilder.java
+++ src/main/java/io/github/lucfr1746/llibrary/itemstack/ItemBuilder.java
@@ -4,9 +4,9 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import io.github.lucfr1746.llibrary.utils.Util;
 import io.github.lucfr1746.llibrary.utils.UtilsString;
+import net.kyori.adventure.platform.bukkit.MinecraftComponentSerializer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
@@ -1184,7 +1184,7 @@ public class ItemBuilder {
     public String getDisplayName() {
         if (isInvalidItemStack()) return null;
         ItemMeta meta = getItemMeta();
-        Component component = LegacyComponentSerializer.legacySection().deserialize(meta.getDisplayName());
+        Component component = MinecraftComponentSerializer.get().deserialize(meta.getDisplayName()).asComponent();
         return MiniMessage.miniMessage().serialize(component);
     }
 
