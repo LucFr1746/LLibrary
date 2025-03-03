cb12876b1c031a7101f76a7e99dfe9f3484a2f1e
Thế Lực <LucFr.1746@gmail.com>
2025-03-03 23:29:14 +0700

    Added action to Requirement

--- src/main/java/io/github/lucfr1746/llibrary/requirement/Requirement.java
+++ src/main/java/io/github/lucfr1746/llibrary/requirement/Requirement.java
@@ -1,35 +1,40 @@
 package io.github.lucfr1746.llibrary.requirement;
 
+import io.github.lucfr1746.llibrary.action.Action;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public abstract class Requirement {
 
     public abstract boolean evaluate(Player player);
 
-//    private ActionHandler acceptHandler;
-//    private ActionHandler denyHandler;
-//
-//    public ActionHandler getDenyHandler() {
-//        return this.denyHandler;
-//    }
-//
-//    public void setDenyHandler(ActionHandler denyHandler) {
-//        this.denyHandler = denyHandler;
-//    }
-//
-//    public boolean hasDenyHandler() {
-//        return this.denyHandler != null;
-//    }
-//
-//    public ActionHandler getAcceptHandler() {
-//        return this.acceptHandler;
-//    }
-//
-//    public void setSuccessHandler(ActionHandler acceptHandler) {
-//        this.acceptHandler = acceptHandler;
-//    }
-//
-//    public boolean hasAcceptHandler() {
-//        return this.acceptHandler != null;
-//    }
+    private List<Action> acceptHandler;
+    private List<Action> denyHandler;
+
+    public List<Action> getDenyHandlers() {
+        return this.denyHandler;
+    }
+
+    public Requirement setDenyHandler(List<Action> denyHandlers) {
+        this.denyHandler = denyHandlers;
+        return this;
+    }
+
+    public boolean hasDenyHandler() {
+        return this.denyHandler != null && !this.denyHandler.isEmpty();
+    }
+
+    public List<Action> getAcceptHandlers() {
+        return this.acceptHandler;
+    }
+
+    public Requirement setSuccessHandler(List<Action> acceptHandlers) {
+        this.acceptHandler = acceptHandlers;
+        return this;
+    }
+
+    public boolean hasAcceptHandler() {
+        return this.acceptHandler != null && !this.acceptHandler.isEmpty();
+    }
 }
