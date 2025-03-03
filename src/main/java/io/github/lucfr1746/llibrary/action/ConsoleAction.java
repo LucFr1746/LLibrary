0cb6fc9005d21636cb6fd5fe9b48ff59a362638b
Thế Lực <LucFr.1746@gmail.com>
2025-03-03 23:32:26 +0700

    Added placeholder in command execution

--- src/main/java/io/github/lucfr1746/llibrary/action/ConsoleAction.java
+++ src/main/java/io/github/lucfr1746/llibrary/action/ConsoleAction.java
@@ -1,12 +1,13 @@
 package io.github.lucfr1746.llibrary.action;
 
+import io.github.lucfr1746.llibrary.utils.Hooks;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class ConsoleAction extends Action {
 
-    private final String command;
+    private String command;
 
     public ConsoleAction(String command) {
         this.command = command;
@@ -16,7 +17,11 @@ public class ConsoleAction extends Action {
         return this.command;
     }
 
-    public void run(Player target) {
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), PlaceholderAPI.setPlaceholders(target, this.command));
+    @Override
+    public void execute(Player target) {
+        if (Hooks.isPAPIEnabled()) {
+            this.command = PlaceholderAPI.setPlaceholders(target, this.command);
+        }
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), this.command);
     }
 }
