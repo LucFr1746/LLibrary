bf0ecc536e777e94206330393646a92fc5b92b63
Thế Lực <LucFr.1746@gmail.com>
2025-03-03 21:10:38 +0700

    Split into load and disable

--- src/main/java/io/github/lucfr1746/llibrary/utils/PluginLoader.java
+++ src/main/java/io/github/lucfr1746/llibrary/utils/PluginLoader.java
@@ -1,16 +1,43 @@
 package io.github.lucfr1746.llibrary.utils;
 
+import dev.jorel.commandapi.CommandAPI;
 import io.github.lucfr1746.llibrary.LLibrary;
+import io.github.lucfr1746.llibrary.inventory.loader.InventoryLoader;
 import io.github.lucfr1746.llibrary.itemstack.gui.ItemBuilderGUI;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PluginLoader {
 
     private final LLibrary plugin;
 
+    private static final List<InventoryLoader> registeredInv = new ArrayList<>();
+
     public PluginLoader(LLibrary plugin) {
         this.plugin = plugin;
+    }
 
+    public PluginLoader loadPlugin() {
         LLibrary.getLoggerAPI().info("Loading ItemBuilder GUI...");
         new ItemBuilderGUI(this.plugin);
+        return this;
+    }
+
+    public PluginLoader disablePlugin() {
+        for (InventoryLoader loader : registeredInv) {
+            unregisterInvCMD(loader);
+        }
+        return this;
+    }
+
+    public static void registerInv(InventoryLoader inventoryLoader) {
+        registeredInv.add(inventoryLoader);
+    }
+
+    private void unregisterInvCMD(InventoryLoader inventoryLoader) {
+        for (String cmd : inventoryLoader.getOpenCommands()) {
+            CommandAPI.unregister(cmd);
+        }
     }
 }
